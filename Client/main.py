9b35c6ee7c8d1b6e2bc58fc0bc16f0ddb6da70d8
SurfinDuck69 <surfinduck69@gmail.com>
2022-11-11 16:36:26 +0100

    a

---
+++ Client/main.py
@@ -0,0 +1,26 @@
+import socket
+import os
+
+
+def client():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+
+        print('Received from server: ' + data)
+
+        message = input(" -> ")
+
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    client()
